c039b24ad6e51401b30dfde899f37f24f450e2f8
DStaferova <daria.staferova@gmail.com>
2015-06-07 17:12:13 +0300

    Added a capability to try out the algorithm on the data added by user

--- create_datasets.py
+++ create_datasets.py
@@ -4,9 +4,6 @@ import apocopes_in_french as aif
 from collections import defaultdict
 from helpers import *
 
-files = [os.path.join(root, file) for root, dirs, files in os.walk("lepoint") for file in files]
-words = defaultdict(int)
-
 rxes = [
     re.compile(u"\s*(Article réservé aux abonnés.*Identifiez-vous)\s*", re.DOTALL|re.U),
     re.compile(u"[^\w']+", re.U),
@@ -16,13 +13,30 @@ def clean(s, rx = re.compile(u"[^\w']+", re.U)):
         s = rx.sub(' ', s)
     return s.lower().strip()
 
-for path in files:
-    with codecs.open(path, 'r', 'utf-8') as f:
-        for word in [word for word in clean(f.read()).split(' ') if word]:
-            words[word] += 1
+def proc(beg):
+    if os.path.isfile(beg):
+        files = [beg]
+    else:
+        files = [os.path.join(root, file) for root, dirs, files in os.walk(beg) for file in files]
+    words = defaultdict(int)
 
-with codecs.open('data/datasets.csv', 'w', 'utf-8') as f:
-    for word, count in sorted(words.items(), key = lambda kv: -kv[1]):
-        f.write(word + delim + str(count) + newline)
+    for path in files:
+        with codecs.open(path, 'r', 'utf-8') as f:
+            for word in [word for word in clean(f.read()).split(' ') if word]:
+                words[word] += 1
+    ordered = sorted(words.items(), key = lambda kv: -kv[1])
+
+    if not words:
+        print 'No data found'
+        return
+
+    data = 'data/datasets.csv'
+    with codecs.open(data, 'w', 'utf-8') as f:
+        for word, count in ordered:
+            f.write(word + delim + str(count) + newline)
     print 'datasets created'
+    print "%s distinct words, %s total words" % (len(words), sum(words.values()))
+    print "most popular: %s (%s occurences)" % (ordered[0][0], ordered[0][1])
 
+if __name__ == "__main__":
+    proc("lepoint")
--- user_interface.py
+++ user_interface.py
@@ -1,41 +1,100 @@
 # coding=utf-8
 import os, sys
+import apocopes_in_french as aif
+import create_datasets as credat
 
-def exit():
-    print "Bye..."
-    sys.exit()
-    pass
+def try_alg():
+    def try_is_apocope():
+        while True:
+            apo = ""
+            print "Enter a potential apocope:"
+            apo = raw_input("> ")
+            if not apo:
+                break
+            fulls = aif.find_fulls(apo)
+            if fulls:
+                print "Potential full forms:"
+                print ', '.join(fulls)
+            else:
+                print "'" + apo + "' is not an apocope"
+            print
 
-actions = [
-    ("Create an apocope dictionary from parsed sources", lambda: os.system("python ./create_dict_apo_auto.py")),
-    ("Create an apocope dictionary augmented with stats", lambda: os.system("python ./create_dict_apo_aug.py")),
-    ("Create a blacklist of cut parts", lambda: os.system("python ./create_blacklist_cut.py")),
-    ("Create a frequency dictionary from le Point articles", lambda: os.system("python ./create_datasets.py")),
-    ("Create a set of comparisons of algorithm improvements based on 'data/list_words'", lambda: os.system("python ./create_imp_comp.py")),
-    ("Exit", exit)
-]
+    def try_is_apocope_of():
+        while True:
+            apo, full = "", ""
+            print "Enter a potential apocope:"
+            apo = raw_input("> ")
+            if not apo:
+                break
+            print "Enter a potential full form:"
+            full = raw_input("> ")
+            if not full:
+                break
+            if aif.is_apocope_of(apo, full):
+                print "'" + apo + "' is an apocope of '" + full + "'"
+            else:
+                print "'" + apo + "' is not an apocope of '" + full + "'"
+            print
 
-def get_choice():
-    print "Hi, here's what we can do:"
-    print "\r\n".join(["    %s. %s" % (i+1, actions[i][0]) for i in range(len(actions))])
-    print
-    print "What would you like to do?"
+    actions_try = [
+        ("Check if the inserted word is an apocope", try_is_apocope),
+        ("Check if the inserted word is an apocope of the inserted full form", try_is_apocope_of),
+        ("Return to the main menu", main_menu),
+    ]
     while True:
-        try:
-            choice = int(raw_input("> "))
-            return choice
-        except:
-            pass
+        exec_action(actions_try)
 
-while True:
-    choice = get_choice()
+def create_own_datasets():
+    print "Enter a path to your data:"
+    p = raw_input("> ")
+    credat.proc(p)
+
+
+def exec_action(acts):
+    def get_choice(acts):
+        print "Here's what we can do:"
+        print "\r\n".join(["    %s. %s" % (i+1, acts[i][0]) for i in range(len(acts))])
+        print
+        print "What would you like to do?"
+        while True:
+            try:
+                choice = int(raw_input("> "))
+                return choice
+            except:
+                pass
+
+    choice = get_choice(acts)
     print
     try:
-        actions[choice-1][1]()
+        acts[choice-1][1]()
     except SystemExit:
-        break
+        raise
     except:
         print "Could not complete the command"
-    print
-    raw_input("Press Enter to continue...")
-    print
+
+
+def main_menu():
+    def exit():
+        print "Bye..."
+        sys.exit()
+
+    actions_main = [
+        ("Create an apocope dictionary from parsed sources", lambda: os.system("python ./create_dict_apo_auto.py")),
+        ("Create an apocope dictionary augmented with stats", lambda: os.system("python ./create_dict_apo_aug.py")),
+        ("Create a blacklist of cut parts", lambda: os.system("python ./create_blacklist_cut.py")),
+        ("Create a frequency dictionary from Le Point articles", lambda: os.system("python ./create_datasets.py")),
+        ("Create a frequency dictionary from your own file", create_own_datasets),
+        ("Create a set of comparisons of algorithm improvements based on 'data/list_words'", lambda: os.system("python ./create_imp_comp.py")),
+        ("Try the algorithm out", try_alg),
+        ("Exit", exit)
+    ]
+    while True:
+        exec_action(actions_main)
+        raw_input("Press Enter to continue...")
+        print
+
+try:
+    print "Hi!"
+    main_menu()
+except SystemExit:
+    pass
